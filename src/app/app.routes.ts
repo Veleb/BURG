aa79cd76151468f6359ad21f5ade51eee652b788
Veleb <philipradev9@gmail.com>
2025-01-06 23:23:48 +0200

    Added basic navigation and routing for some pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+
+  { path: "home", component: HomeComponent },
+  { path: "contact", component: ContactComponent },
+  
+  { path: "404", component: PageNotFoundComponent },
+  { path: "**", redirectTo: "/404", pathMatch: "full" }
+];
