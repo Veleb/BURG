6c0e2ed805635620d8f0ee61f9ae8c8376470997
Veleb <philipradev9@gmail.com>
2025-01-20 22:21:32 +0200

    fixed cookies

--- Server/src/utils/setAuthTokens.ts
+++ Server/src/utils/setAuthTokens.ts
@@ -1,8 +1,22 @@
 import { Response } from "express";
 
 const setAuthTokens = (res: Response, accessToken: string, refreshToken: string): void => {
-  res.cookie('refresh_token', refreshToken, { httpOnly: true, secure: true, sameSite: 'none' });
-  res.cookie('auth', accessToken, { httpOnly: true, secure: true, sameSite: 'none' });
+  const isProd = process.env.PROD === 'true';
+  const sameSitePolicy = ( isProd ? 'none' : 'lax' );
+
+  res.cookie('auth', accessToken, {
+    httpOnly: true, 
+    secure: isProd, 
+    sameSite: sameSitePolicy, 
+    maxAge: 1000 * 60 * 15,
+  });
+
+  res.cookie('refresh_token', refreshToken, {
+      httpOnly: true, 
+      secure: isProd, 
+      sameSite: sameSitePolicy, 
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
 };
 
 export default setAuthTokens;
